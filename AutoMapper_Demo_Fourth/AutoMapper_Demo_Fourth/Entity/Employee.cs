ee3e85e1aab65870a3fa0f86df9a588954d52d36
samirkhan-947 <samirrkhan947@gmail.com>
2023-02-07 13:20:25 +0530

    Mapping Complex type to Primitive Type using AutoMapper in C#

---
+++ AutoMapper_Demo_Fourth/AutoMapper_Demo_Fourth/Entity/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper_Demo_Fourth.Entity
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public string JobsName { get; set; }
+        public string Location { get; set; }
+    }
+    public class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class Jobs
+    {
+        public string JobsName { get; set; }
+        public string Location { get; set; }
+
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public Address address { get; set; }
+        public Jobs job { get; set; }
+
+    }
+}
---
+++ AutoMapper_Demo_Fourth/AutoMapper_Demo_Fourth/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+using AutoMapper_Demo_Fourth.Entity;
+
+Console.WriteLine("Hello, World!");
+Address empAddres = new Address()
+{
+    City = "Mumbai",
+    State = "Maharashtra",
+    Country = "India"
+};
+Employee emp = new Employee();
+emp.Name = "James";
+emp.Salary = 20000;
+emp.Department = "IT";
+emp.City = "Mumbai";
+emp.State = "Maharashtra";
+emp.Country = "India";
+emp.JobsName = "IT";
+emp.Location = "New Delhi";
+
+
+Jobs Empjob = new Jobs()
+{ 
+JobsName = "IT",
+Location = "New Delhi"
+};
+
+var mapper = InitializeAutomapper();
+var empDTO = mapper.Map<EmployeeDTO>(emp);
+
+Console.WriteLine("Name:" + empDTO.Name + ", Salary:" + empDTO.Salary + ", Department:" + empDTO.Department);
+Console.WriteLine("City:" + empDTO.address.City + ", State:" + empDTO.address.State + ", Country:" + empDTO.address.Country);
+Console.WriteLine("JobsName:" + empDTO.job.JobsName + ", Location:" + empDTO.job.Location);
+
+Console.ReadLine();
+
+static Mapper InitializeAutomapper()
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<Employee, EmployeeDTO>()
+        .ForMember(dest => dest.address, act => act.MapFrom(src => new Address()
+        {
+            City = src.City,
+            State = src.State,
+            Country = src.Country
+        })).ForMember(dest => dest.job, act => act.MapFrom(src => new Jobs()
+        {
+            JobsName = src.JobsName,
+            Location = src.Location
+        }));
+        //cfg.CreateMap<Employee, EmployeeDTO>()
+        //.ForMember(dest => dest.job, act => act.MapFrom(src =>  new Jobs()
+        //{
+        //    JobsName = src.JobsName,
+        //    Location = src.Location 
+        //}));
+    });
+    var mapper = new  Mapper(config);
+    return mapper;
+} 
