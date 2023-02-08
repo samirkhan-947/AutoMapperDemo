d7757ea964d4319e05b63fc0691ef9bea5b0b139
samirkhan-947 <samirrkhan947@gmail.com>
2023-02-08 12:57:35 +0530

    Impmented Igonre Property Seven part One

---
+++ AutoMapper_Seven_PartOne/AutoMapper_Seven_PartOne/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+
+Console.WriteLine("Hello, World!");
+
+var mapper = InitializeAutomapper();
+Employee employee = new Employee()
+{
+    ID = 101,
+    Name = "James",
+    Address = "Mumbai"
+};
+var empDTO = mapper.Map<Employee, EmployeeDTO>(employee);
+Console.WriteLine("After Mapping : Employee");
+Console.WriteLine("ID : " + employee.ID + ", Name : " + employee.Name + ", Address : " + employee.Address);
+Console.WriteLine();
+Console.WriteLine("After Mapping : EmployeeDTO");
+Console.WriteLine("ID : " + empDTO.ID + ", Name : " + empDTO.Name + ", Address : " + empDTO.Address);
+Console.ReadLine();
+static Mapper InitializeAutomapper()
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<Employee, EmployeeDTO>()
+            //Ignoring the Address property of the destination type
+            .ForMember(dest => dest.Address, act => act.Ignore());
+    });
+    var mapper = new Mapper(config);
+    return mapper;
+}
+public class Employee
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
+public class EmployeeDTO
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
