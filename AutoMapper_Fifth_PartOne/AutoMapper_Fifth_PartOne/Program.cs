104c0b915f64e4399a96033ff379e8a7edfc960c
samirkhan-947 <samirrkhan947@gmail.com>
2023-02-08 12:26:04 +0530

    Implement Reverse Mapping part one

---
+++ AutoMapper_Fifth_PartOne/AutoMapper_Fifth_PartOne/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+
+Console.WriteLine("Hello, World!");
+
+
+//Step1: Initialize the Mapper
+var mapper = InitializeAutomapper();
+//Step2: Create the Order Request
+var OrderRequest = CreateOrderRequest();
+//Step3: Map the OrderRequest object to Order DTO
+var orderDTOData = mapper.Map<Order, OrderDTO>(OrderRequest);
+//Step4: Print the OrderDTO Data
+Console.WriteLine("After Mapping - OrderDTO Data");
+Console.WriteLine("OrderId : " + orderDTOData.OrderId);
+Console.WriteLine("NumberOfItems : " + orderDTOData.NumberOfItems);
+Console.WriteLine("TotalAmount : " + orderDTOData.TotalAmount);
+Console.WriteLine("CustomerId : " + orderDTOData.CustomerId);
+Console.WriteLine("Name : " + orderDTOData.Name);
+Console.WriteLine("Postcode : " + orderDTOData.Postcode);
+Console.WriteLine("MobileNo : " + orderDTOData.MobileNo);
+Console.WriteLine();
+//Step5: modify the OrderDTO data
+orderDTOData.OrderId = 10;
+orderDTOData.NumberOfItems = 20;
+orderDTOData.TotalAmount = 2000;
+orderDTOData.CustomerId = 5;
+orderDTOData.Name = "Smith";
+orderDTOData.Postcode = "12345";
+//Step6: Reverse Map
+mapper.Map(orderDTOData, OrderRequest);
+
+//Step7: Print the Order Data
+Console.WriteLine("After Reverse Mapping - Order Data");
+Console.WriteLine("OrderNo : " + OrderRequest.OrderNo);
+Console.WriteLine("NumberOfItems : " + OrderRequest.NumberOfItems);
+Console.WriteLine("TotalAmount : " + OrderRequest.TotalAmount);
+Console.WriteLine("CustomerId : " + OrderRequest.Customer.CustomerID);
+Console.WriteLine("FullName : " + OrderRequest.Customer.FullName);
+Console.WriteLine("Postcode : " + OrderRequest.Customer.Postcode);
+Console.WriteLine("ContactNo : " + OrderRequest.Customer.ContactNo);
+Console.ReadLine();
+
+ static Order CreateOrderRequest()
+{
+    return new Order
+    {
+        OrderNo = 101,
+        NumberOfItems = 3,
+        TotalAmount = 1000,
+        Customer = new Customer()
+        {
+            CustomerID = 777,
+            FullName = "James Smith",
+            Postcode = "755019",
+            ContactNo = "1234567890"
+        },
+    };
+}
+
+static Mapper InitializeAutomapper()
+{
+    var config = new MapperConfiguration(cfg => {
+        cfg.CreateMap<Order, OrderDTO>()
+            //OrderId is different so map them using For Member
+            .ForMember(dest => dest.OrderId, act => act.MapFrom(src => src.OrderNo))
+            //Customer is a Complex type, so Map Customer to Simple type using For Member
+            .ForMember(dest => dest.Name, act => act.MapFrom(src => src.Customer.FullName))
+            .ForMember(dest => dest.Postcode, act => act.MapFrom(src => src.Customer.Postcode))
+            .ForMember(dest => dest.MobileNo, act => act.MapFrom(src => src.Customer.ContactNo))
+            .ForMember(dest => dest.CustomerId, act => act.MapFrom(src => src.Customer.CustomerID))
+            .ReverseMap();
+    });
+
+    var mapper = new Mapper(config);
+    return mapper;
+}
+    
+
+public class Order
+{
+    public int OrderNo { get; set; }
+    public int NumberOfItems { get; set; }
+    public int TotalAmount { get; set; }
+    public Customer Customer { get; set; }
+}
+public class Customer
+{
+    public int CustomerID { get; set; }
+    public string FullName { get; set; }
+    public string Postcode { get; set; }
+    public string ContactNo { get; set; }
+}
+public class OrderDTO
+{
+    public int OrderId { get; set; }
+    public int NumberOfItems { get; set; }
+    public int TotalAmount { get; set; }
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Postcode { get; set; }
+    public string MobileNo { get; set; }
+}
